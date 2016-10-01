63c6538f4d150636c7e5ac017ffa5c56ca70af7e
Matt F <techmatt@gmail.com>
2016-10-01 15:49:01 -0700

    .

--- grandLARP/grandLARP/Form1.cs
+++ grandLARP/grandLARP/Form1.cs
@@ -16,5 +16,10 @@ namespace grandLARP
         {
             InitializeComponent();
         }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Maze m = new Maze();
+        }
     }
 }
--- grandLARP/grandLARP/Maze.cs
+++ grandLARP/grandLARP/Maze.cs
@@ -1,33 +1,180 @@
 ﻿using System;
+using System.IO;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Drawing;
+using System.Drawing.Drawing2D;
 
 namespace grandLARP
 {
-    struct Vertex
+    static class Constants
+    {
+        public const int imgWidth = 640;
+        public const int imgHeight = 480;
+
+        public const string baseDir = @"C:\Code\puppy-planners\grandLARP\";
+        public const string dataDir = baseDir + @"data\";
+        public const string landscapeDir = baseDir + @"art\landscapes\";
+        public const string symbolDir = baseDir + @"art\symbols\";
+        public const string outDir = baseDir + @"out\";
+    }
+    class Vertex
     {
         public int x;
         public int y;
-        public int radius;
         public string trueID;
         public string sampledID;
+        public string publicName, internalName, desc, symbol, image;
+        public bool isBorder = true;
 
         public void resampleID()
         {
 
         }
+
+        public Bitmap loadImage()
+        {
+            Bitmap bmpIn = new Bitmap(Image.FromFile(Constants.landscapeDir + image));
+            var bmpOut = new Bitmap((int)Constants.imgWidth, (int)Constants.imgHeight);
+            var graph = Graphics.FromImage(bmpOut);
+            var brush = new SolidBrush(Color.Black);
+
+            graph.InterpolationMode = InterpolationMode.High;
+            graph.CompositingQuality = CompositingQuality.HighQuality;
+            graph.SmoothingMode = SmoothingMode.AntiAlias;
+
+            float scale = Math.Min(Constants.imgWidth / bmpIn.Width, Constants.imgHeight / bmpIn.Height);
+            var scaleWidth = (int)(bmpIn.Width * scale);
+            var scaleHeight = (int)(bmpIn.Height * scale);
+
+            graph.FillRectangle(brush, new RectangleF(0, 0, Constants.imgWidth, Constants.imgHeight));
+            graph.DrawImage(bmpOut, new Rectangle(((int)Constants.imgWidth - scaleWidth) / 2, ((int)Constants.imgHeight - scaleHeight) / 2, scaleWidth, scaleHeight));
+
+            return bmpOut;
+        }
     }
 
     class Maze
     {
-        Maze()
+        public void loadID()
         {
-            radiusLetters.Add('A'); // 0
-            radiusLetters.Add('B'); // 1
-            radiusLetters.Add('C'); // 2
-            radiusLetters.Add('Z'); // 3
+            var lines = File.ReadAllLines(Constants.dataDir + "trueIDMap.txt");
+            for (int i = 0; i < 7; i++)
+            {
+                for (int j = 0; j < 7; j++)
+                {
+                    Vertex v = new Vertex();
+                    v.trueID = "Z";
+                    v.x = i;
+                    v.y = j;
+                    vertices[i, j] = v;
+                }
+            }
+            for (int i = 0; i < 5; i++)
+            {
+                for(int j = 0; j < 5; j++)
+                {
+                    vertices[i + 1, j + 1].trueID = lines[i][j].ToString();
+                    vertices[i + 1, j + 1].isBorder = false;
+                }
+            }
+        }
+
+        public void loadInfo()
+        {
+            var lines = File.ReadAllLines(Constants.dataDir + "placeInfo.txt");
+            Vertex activeVertex = null;
+            foreach (string line in lines)
+            {
+                
+                if(line.Contains("=") && !line.StartsWith("#"))
+                {
+                    //id=A
+                    //internalName=almanac
+                    //name=A peaceful cave
+                    //desc=Description!There is stuff here.
+                    //image=almnac.jpg
+                    //symbol=a.jpg
+                    var parts = line.Split('=');
+                    var p = parts[0];
+                    var v = parts[1];
+                    if (p == "id")
+                    {
+                        foreach (var vertex in vertices)
+                            if (vertex.trueID == v)
+                                activeVertex = vertex;
+                    }
+                    if (p == "internalName")
+                        activeVertex.internalName = v;
+                    if (p == "name")
+                        activeVertex.publicName = v;
+                    if (p == "desc")
+                        activeVertex.desc = v;
+                    if (p == "image")
+                        activeVertex.image = v;
+                    if (p == "symbol")
+                        activeVertex.symbol = v;
+                }
+            }
+        }
+
+        public void makeGMImage()
+        {
+            const int buffer = 10;
+            const int wSize = (int)Constants.imgWidth + buffer;
+            const int hSize = (int)Constants.imgHeight + buffer;
+            var imgOut = new Bitmap(wSize * 5, hSize * 5);
+            var graph = Graphics.FromImage(imgOut);
+
+            for (int i = 0; i < 5; i++)
+            {
+                for (int j = 0; j < 5; j++)
+                {
+                    Vertex v = vertices[i + 1, j + 1];
+                    Bitmap vImg = v.loadImage();
+
+                    graph.DrawImage(vImg, new Point(i * wSize, j * hSize));
+                }
+            }
+
+            imgOut.Save(Constants.outDir + "GMImage.png");
+        }
+
+        public Maze()
+        {
+            vowels.Add('A');
+            vowels.Add('E');
+            vowels.Add('I');
+            vowels.Add('O');
+            vowels.Add('U');
+            vowels.Add('Y');
+
+            consonants.Add('B');
+            consonants.Add('C');
+            consonants.Add('D');
+            consonants.Add('F');
+            consonants.Add('G');
+            consonants.Add('H');
+            consonants.Add('J');
+            consonants.Add('K');
+            consonants.Add('L');
+            consonants.Add('M');
+            consonants.Add('N');
+            consonants.Add('P');
+            consonants.Add('Q');
+            consonants.Add('R');
+            consonants.Add('S');
+            consonants.Add('T');
+            consonants.Add('V');
+            consonants.Add('W');
+            consonants.Add('X');
+            consonants.Add('Y');
+            
+            loadID();
+            loadInfo();
+            makeGMImage();
 
             // ******* 3333333 ******* 6543456 3333333
             // *ABCDE* 3012113 *A+T+N* 5432345 3222223
@@ -53,8 +200,8 @@ namespace grandLARP
             // R is the radius, L is the label, I is the 'interesting' flag (vowel is good, consnant is boring)
             // presence of a Z anywhere is really bad
         }
-        public List<char> critialLetters = new List<char>();
-        public List<char> randomLetters = new List<char>();
-        public List<char> radiusLetters = new List<char>();
+        public List<char> vowels = new List<char>();
+        public List<char> consonants = new List<char>();
+        public Vertex[,] vertices = new Vertex[7, 7];
     }
 }
